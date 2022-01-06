34b4c67a45cc3fe7d1f9c5313be9fbee712fff20
Marco E <31892221+marcoandre1@users.noreply.github.com>
2022-01-06 00:51:14 -0500

    Update demo.js

--- scripts/demo.js
+++ scripts/demo.js
@@ -2,72 +2,72 @@
 
 /* This code 
    creates an 
-   console.log box */
+   alert box */
 
 function myFunction() {
-  console.log("Calling a Function!");
+  alert("Calling a Function!");
 }
 
 myFunction();
 //Alerts "Calling a Function!"
 
 function sayHello(name) {
-   console.log("Hi, " + name);
+   alert("Hi, " + name);
 }
 
 sayHello("Marco");
 //Alerts "Hi, Marco"
 
-console.log("Hello\nHow are you?");
+alert("Hello\nHow are you?");
 
-// var user = prompt("Please enter your name", "name");
-// console.log(user);
+var user = prompt("Please enter your name", "name");
+alert(user);
 
-// var result = confirm("Do you really want to leave this page?");
-// if (result == true) {
-//   console.log("Thanks for visiting");
-// }
-// else {
-//   console.log("Thanks for staying with us");
-// }
+var result = confirm("Do you really want to leave this page?");
+if (result == true) {
+  alert("Thanks for visiting");
+}
+else {
+  alert("Thanks for staying with us");
+}
 
-// var x = 100;
-// document.write(x + "<br />");
+var x = 100;
+document.write(x + "<br />");
 
-// var price = 55.55;
-// document.write(price + "<br />");
+var price = 55.55;
+document.write(price + "<br />");
 
-// var sayHello = 'Hello world! \'I am a JavaScript programmer.\' ';
-// document.write(sayHello + "<br />");
+var sayHello = 'Hello world! \'I am a JavaScript programmer.\' ';
+document.write(sayHello + "<br />");
 
-// var y = 56%3;
-// document.write(y + "<br />");
+var y = 56%3;
+document.write(y + "<br />");
 
-// for (i=1; i<=5; i++) {
-//    document.write(i + "<br />");
-// }
+for (i=1; i<=5; i++) {
+   document.write(i + "<br />");
+}
 
-// var i=0;
-// while (i<=10) {
-//    document.write(i + "<br />");
-//    i++;
-// }
+var i=0;
+while (i<=10) {
+   document.write(i + "<br />");
+   i++;
+}
 
-// var sum=0; 
-// while(sum<6) {
-//   sum++;
-// }
-// document.write(sum);
+var sum=0; 
+while(sum<6) {
+  sum++;
+}
+document.write(sum);
 
-// function test(number)
-// {
-//    while(number < 5) {
-//       number++;
-//    }
-//    return number;
-// }
-// console.log(test(2));
+function test(number)
+{
+   while(number < 5) {
+      number++;
+   }
+   return number;
+}
+alert(test(2));
 
-// var n = prompt("Enter a number", "");
-// var answer = Math.sqrt(n);
-// console.log("The square root of " + n + " is " + answer);
+var n = prompt("Enter a number", "");
+var answer = Math.sqrt(n);
+alert("The square root of " + n + " is " + answer);
